6b9f8d10c1fff649a109ed96b7bbe82c0bc8718c
fede4ka <40442383+fede4ka@users.noreply.github.com>
2019-02-26 23:16:00 +0300

    Update Service.scala

--- src/main/scala/Service.scala
+++ src/main/scala/Service.scala
@@ -12,8 +12,6 @@ import spray.json._
 import methods._
 
 object Service extends App {
-
-
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
@@ -37,13 +35,9 @@ object Service extends App {
         }
       }
 
-
           val zipped = tags zip res
-
           val data = ByteString(zipped.toJson.toString())
           HttpResponse( entity = HttpEntity(ContentTypes.`application/json`, data))
-
-      
       }}
     val bindingFuture = Http().bindAndHandleSync(requestHandler, "localhost", 8080)
     println(s"Started...\nTest request link http://localhost:8080/search?tag=clojure&tag=haskell&tag=java\nPress RETURN to stop...")
